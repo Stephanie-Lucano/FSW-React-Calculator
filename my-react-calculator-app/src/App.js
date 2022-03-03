822304bf6652a325b23e8d4fa28f940ff5a60599
stephanie-lucano <stephaniemarielucano@pursuit.org>
2022-03-03 09:08:01 -0500

    Added +/- and DEL button

--- my-react-calculator-app/src/App.js
+++ my-react-calculator-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [operation, setOperation] = useState("")
   const [result, setResult] = useState(0)
 
-  const operators = ["/", "*", "+", "-"]
+  const operators = ["/", "*", "+", "-", "."]
 
   const updateCalc = (value) => {
     // guard clause
@@ -51,6 +51,14 @@ function App() {
     setResult("")
   }
 
+  const handleSign = () => {
+    setDisplayValue(displayValue * -1)
+  }
+
+  const handleDelete = () => {
+    setDisplayValue("")
+  }
+
   return (
     <div className="App">
       <div className="calculator">
@@ -61,38 +69,44 @@ function App() {
         </div>
 
         <div className="buttons">
+          <button onClick={handleDelete}
+          >DEL</button>
+          <button onClick={handleAllClear}
+          >AC</button>
+          <button onClick={handleSign}
+          >+/-</button>
+          <button onClick={() => handleOperator("/")}
+          >/</button>
           <button onClick={() => updateCalc("7")}
           >7</button>
           <button onClick={() => updateCalc("8")}
           >8</button>
           <button onClick={() => updateCalc("9")}
           >9</button>
-          <button onClick={() => handleOperator("+")}
-          >+</button>
+          <button onClick={() => handleOperator("*")}
+          >*</button>
           <button onClick={() => updateCalc("4")}
           >4</button>
           <button onClick={() => updateCalc("5")}
           >5</button>
           <button onClick={() => updateCalc("6")}
           >6</button>
-          <button onClick={() => handleOperator("-")}
-          >-</button>
+          <button onClick={() => handleOperator("+")}
+          >+</button>
           <button onClick={() => updateCalc("1")}
           >1</button>
           <button onClick={() => updateCalc("2")}
           >2</button>
           <button onClick={() => updateCalc("3")}
           >3</button>
-          <button onClick={() => handleOperator("*")}
-          >*</button>
-          <button onClick={handleAllClear}
-          >AC</button>
+          <button onClick={() => handleOperator("-")}
+          >-</button>
           <button onClick={() => updateCalc("0")}
           >0</button>
+          <button onClick={() => updateCalc(".")}
+          >.</button>
           <button onClick={handleResult}
           >=</button>
-          <button onClick={() => handleOperator("/")}
-          >/</button>
         </div>
 
       </div>
